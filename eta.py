5441861ab88154b0ce0f9582ae446c946438dc4c
nar2p1nk <farrisisqandar@gmail.com>
2022-02-16 23:30:22 +0800

    completed eta main function

---
+++ eta.py
@@ -0,0 +1,13 @@
+
+
+def distance(time,speed):
+    distance = time * speed
+    return distance
+
+def speed(distance,time):
+    speed = distance / time
+    return speed
+
+def time(distance,speed):
+    time = distance / speed
+    return time
